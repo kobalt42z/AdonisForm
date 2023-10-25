e150872bd67be556b3bda59a9d980339b129a516
Shmouel kessous <chmouel2690@gmail.com>
2023-10-25 13:26:48 +0300

    fix

--- database/migrations/1695038074602_roles.ts
+++ database/migrations/1695038074602_roles.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.increments('id') 
       table.string("name",30) 
       table.string("description",255)
       /**
